6a6f6b33f36ed741bbdbb7ef2c7d99b079fef3db
Sebastian Trujillo <129899570+strujilloa@users.noreply.github.com>
2026-04-09 09:15:42 -0500

    Update empaquetados

--- src/config/global.js
+++ src/config/global.js
@@ -135,7 +135,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/33130235_CF2_DU.pdf',
+        download: 'downloads/33130245_CF2_DU.pdf',
       },
       {
         icono: 'fas fa-download',
